4735174fe0fea54696a9a4ec085d25c6c7f98b83
Eric Hayth <ehayth@cloudflare.com>
2024-05-08 16:41:10 -0400

    update 3

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ addEventListener('fetch', event => {
 	const botScore = request.cf.botManagement.score
 	//Check if the bot score is less than 30
 	if (botScore && parseInt(botScore) < 30) {
-		//Redirect to httpbin.
-	  return fetch('https://httpbin.com/get');
+		//fetch to httpbin.
+	  return fetch('https://httpbin.org/get');
 	  ;
 	} else {
 	  // Pass GET request traffic to the origin
