d9ad2234b265dd33c3bc76105f1f8c4439b69209
Eric Hayth <ehayth@cloudflare.com>
2024-05-08 16:50:51 -0400

    update 5

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,9 @@ addEventListener('fetch', event => {
 	  // Pass GET request traffic to the origin
 	  if (request.method === 'GET') {
 		return fetch(request);
-	  } else {
-		// Handle non-GET requests directly in the worker
+	  } 
+	  else {
+		// Respond with a 405 for non-GET requests
 		return new Response('Method Not Allowed', {
 		  status: 405,
 		  headers: { 'Content-Type': 'text/plain' },
